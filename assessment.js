cbd79a17d873f6571de81f978c0306c83a47fd77
FRCB <france.bushman@gmail.com>
2018-03-29 12:39:07 -0400

    ready assessment

--- assessment.js
+++ assessment.js
@@ -12,7 +12,7 @@ var lovesCode = true;
 
 // #4 Using the previously created variables, create a new variable called 'me' that combines the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
 
-var me = "My name is " + myName + " and my favorite number is " + faveNum ;
+var me = "My name is " + myName + " and my favorite number is " + faveNum;
 
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum' that outputs true when comparing faveNum to your favorite number.
 
@@ -20,7 +20,7 @@ var faveNum = 7;
 var trueFaveNum;
 
 if (7 === faveNum) {
-trueFaveNum = true
+  trueFaveNum = true
 };
 
 // #6 Create a new variable called notHakeem using the previously created myName variable, test that your name DOES NOT equal 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
@@ -40,34 +40,34 @@ var age = 16;
 var canDrive;
 
 if (age >= 16) {
-canDrive = true;
+  canDrive = true;
 } else if (age < 16) {
-canDrive = false
+  canDrive = false
 };
 
 // #8 Create an if statement inside of the responseCreator function that sets the colorResponse variable value based off of a passed in value. The param value passed in will be added in the unit test. If the param value is 'green' set colorResponse variable to 'Green is okay'. If the param value is 'red' set reponse variable to 'I heart red'. Otherwise set the colorResponse variable to 'What is your favorite color?'. Make sure to return the colorResponse variable at the end once it has been properly set.
 
 var colorResponse;
-var responseCreator = function(param){
-if (param === "green") {
-colorResponse = "Green is okay";
-} else if (param === "red") {
-colorResponse = "I heart red";
-} else {
-colorResponse = "What is your favorite color?";
-} 
-return colorResponse
+var responseCreator = function (param) {
+  if (param === "green") {
+    colorResponse = "Green is okay";
+  } else if (param === "red") {
+    colorResponse = "I heart red";
+  } else {
+    colorResponse = "What is your favorite color?";
+  }
+  return colorResponse
 };
 
 // #9 Create a function called 'sum' that takes in a parameter and returns the sum of that parameter.
 
 function sum(param) {
-total = param + param
-return total
+  total = param + param
+  return total
 };
 
 // #10 Create a function called greeter that takes in two parameters, name(a string) and number(a number). Have greeter return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values.
 
 function greeter(name, number) {
   return "Hi! I am " + name + " and I am " + number + " years old"
-  };
+};
